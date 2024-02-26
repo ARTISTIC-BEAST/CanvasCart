569a1441c5ebb6c101100bc57c3d4d54ea9d0817
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2024-02-26 11:03:16 -0800

    Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <a href="http://www.artisticbeast.dev" target="_blank" rel="noopener noreferrer">Created by ARTISTIC BEAST</a>
+        </div>
+    )
+}
