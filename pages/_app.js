7f992cf0667cdd3524c5e2b05feeb671fb23a5eb
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2024-02-26 10:13:27 -0800

    _app.js

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/styles.css'
+import React from "react";
+import { AuthUserContextProvider } from '../data/context/AuthUserContext';
+import { ProductProvider } from '../data/context/ProductContext';
+
+export default function App({ Component, pageProps }) {
+    return (  
+      <div>
+        <AuthUserContextProvider>
+            <ProductProvider>
+                <Component {...pageProps} />
+            </ProductProvider>
+        </AuthUserContextProvider>
+      </div>
+      )
+  }
